00593ac0321486d1d50fa953d2a53a80b23fc939
lawwm <underworlddonutmerchant@gmail.com>
2022-12-29 01:00:14 +0800

    Add basic code

---
+++ jumper/game.h
@@ -0,0 +1,20 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "player.h"
+
+class Graphics;
+
+class Game {
+public:
+	Game();
+	~Game();
+private:
+	void gameLoop();
+	void draw(Graphics& graphics);
+	void update(float elapsedTime);
+
+	Player _player;
+};
+
+#endif
---
+++ jumper/main.cpp
@@ -0,0 +1,13 @@
+/*
+Main.cpp
+Entry point
+*/
+#define SDL_MAIN_HANDLED
+#include "SDL.h"
+#include "game.h"
+
+
+int main() {
+	Game game;
+	return 0;
+}
